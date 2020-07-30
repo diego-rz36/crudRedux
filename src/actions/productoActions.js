c45510c63a61a482d2c6b5d2d840ab7dac756b09
Diego <diegorz36@gmail.com>
2020-07-29 21:59:04 -0500

    AgregarProductos

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -3,9 +3,37 @@ import {
     AGREGAR_PRODUCTO_EXITO,
     AGREGAR_PRODUCTO_ERROR
 } from '../types';
+import clienteAxios from '../config/axios';
 
 export function crearNuevoProductoAction(producto){
-    return() => {
-        console.log(producto);
+    return async (dispatch) => {
+        dispatch(agregarProducto());
+
+        try{
+            //Insertar API
+            await clienteAxios.post('/productos', producto);
+
+            //Actualiza state
+            dispatch(agregarProductoExito(producto));
+        } catch(error){
+            console.log(error);
+            //Cambia el state si hay error
+            dispatch(agregarProductoError(true));
+        }
     }
 }
+
+const agregarProducto = () => ({
+    type: AGREGAR_PRODUCTO,
+    payload: true
+})
+
+const agregarProductoExito = producto =>({
+    type: AGREGAR_PRODUCTO_EXITO,
+    payload: producto
+})
+
+const agregarProductoError = estado =>({
+    type: AGREGAR_PRODUCTO_ERROR,
+    payload: estado
+})
