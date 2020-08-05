55a624700821a4f37d2a7027bc0436d1b95becfa
Diego <diegorz36@gmail.com>
2020-08-04 22:07:56 -0500

    Terminacion agregar productos

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,9 +1,29 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect} from 'react';
+
+import {useDispatch, useSelector} from 'react-redux';
+import { obtenerPorductosAction } from '../actions/productoActions'
+import Producto from './Producto';
 
 const Productos = () => {
+
+    const dispatch = useDispatch();
+
+
+    useEffect(()=> {
+        const cargarProductos = () => dispatch(obtenerPorductosAction());
+        cargarProductos();
+    }, []);
+
+    const productos = useSelector(state => state.productos.productos);
+    const error = useSelector( state => state.productos.error);    
+    const cargando = useSelector( state => state.productos.loading);
     return (
         <Fragment>
             <h2 className="text-center my-5">Listado de Productos</h2>
+
+            {error ? <p className="alert alert-danger font-weight-bold text-center mt-4">Hubo un error.</p> :null}
+            {cargando ? <p className="text-center">Cargando...</p> :null}
+
             <table className="table table-striped">
                 <thead className="bg-primary table-dark">
                     <tr>
@@ -13,7 +33,14 @@ const Productos = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    
+                    {productos.length === 0 ? 'No hay productos' :
+                        productos.map(producto => (
+                            <Producto
+                                key={producto.id}
+                                producto={producto}
+                            />
+                        ))
+                    }
                 </tbody>
             </table>
         </Fragment>
--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -1,20 +1,25 @@
 import {
     AGREGAR_PRODUCTO,
     AGREGAR_PRODUCTO_EXITO,
-    AGREGAR_PRODUCTO_ERROR
+    AGREGAR_PRODUCTO_ERROR,
+    COMENZAR_DESCARGA_PRODUCTOS,
+    DESCARGA_PRODUCTOS_EXITO,
+    DESCARGA_PRODUCTOS_ERROR
 } from '../types';
 //cada reducer tiene su propio state
 
 const initialState = {
     productos: [],
     error: null,
-    loading: false
+    loading: false,
+    productoeliminar: null
 }
 
 
 
 export default function(state = initialState, action){
     switch(action.type){
+        case COMENZAR_DESCARGA_PRODUCTOS:
         case AGREGAR_PRODUCTO:
             return {
                 ...state,
@@ -26,11 +31,19 @@ export default function(state = initialState, action){
                 loading: false,
                 productos: [...state.productos, action.payload]
             }
+        case DESCARGA_PRODUCTOS_ERROR:
         case AGREGAR_PRODUCTO_ERROR:
             return {
                 ...state,
                 loading: false,
                 error: action.payload
+            }
+        case DESCARGA_PRODUCTOS_EXITO:
+            return {
+                ...state,
+                loading: false,
+                error: null,
+                productos: action.payload
             }
         default:
             return state;
